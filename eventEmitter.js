05a4be6f61d1df45b1e4e99b4423c7e051385efe
Suraj Gupta <8700suraj@gmail.com>
2024-11-23 21:29:22 +0530

    Event,Emitter

---
+++ eventEmitter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const EvenEmitter = require('events');
+const app = express();
+const events = new EvenEmitter();
+
+let count = 0;
+
+events.on('countAPI',()=>{
+    count++;
+    console.log('event called',count);
+})
+
+app.get('/', (req,resp)=>{
+    resp.send('api called');
+    events.emit("countAPI");
+});
+app.get('/search', (req,resp)=>{
+    resp.send('complete api called');
+    events.emit("countAPI");
+
+});
+app.get('/complete', (req,resp)=>{
+    resp.send('complete api called');
+    events.emit("countAPI");
+});
+
+app.listen(5000);
